d8b717b813b605cc6a4384bc39cab9b8fc083161
mulen <mulendajudith2018@gmail.com>
2025-10-30 20:53:55 +0200

    added the unit test for the logics

---
+++ GiftOfTheGivers.Tests/LogicTests/DonationServiceTests.cs
@@ -0,0 +1,41 @@
+﻿using DisasterAlleviationPortal.Logic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GiftOfTheGivers.Tests
+{
+    [TestClass]
+    public class DonationServiceTests
+    {
+        [TestMethod]
+        public void ValidateDonation_ValidInput_ReturnsTrue()
+        {
+            var service = new DonationService();
+            var result = service.ValidateDonation("Water", 10);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void ValidateDonation_ZeroQuantity_ReturnsFalse()
+        {
+            var service = new DonationService();
+            var result = service.ValidateDonation("Water", 0);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void ValidateDonation_NegativeQuantity_ReturnsFalse()
+        {
+            var service = new DonationService();
+            var result = service.ValidateDonation("Water", -5);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void ValidateDonation_EmptyItem_ReturnsFalse()
+        {
+            var service = new DonationService();
+            var result = service.ValidateDonation("", 5);
+            Assert.IsFalse(result);
+        }
+    }
+}
---
+++ GiftOfTheGivers.Tests/LogicTests/LoginValidatorTests.cs
@@ -0,0 +1,41 @@
+﻿using DisasterAlleviationPortal.Logic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GiftOfTheGivers.Tests
+{
+    [TestClass]
+    public class LoginValidatorTests
+    {
+        [TestMethod]
+        public void ValidateCredentials_ValidInput_ReturnsTrue()
+        {
+            var validator = new LoginValidator();
+            var result = validator.ValidateCredentials("user@example.com", "secure123");
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void ValidateCredentials_EmptyEmail_ReturnsFalse()
+        {
+            var validator = new LoginValidator();
+            var result = validator.ValidateCredentials("", "secure123");
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void ValidateCredentials_ShortPassword_ReturnsFalse()
+        {
+            var validator = new LoginValidator();
+            var result = validator.ValidateCredentials("user@example.com", "123");
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void ValidateCredentials_InvalidEmailFormat_ReturnsFalse()
+        {
+            var validator = new LoginValidator();
+            var result = validator.ValidateCredentials("userexample.com", "secure123");
+            Assert.IsFalse(result);
+        }
+    }
+}
